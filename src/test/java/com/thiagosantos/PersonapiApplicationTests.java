08191f5bebae92de32e585850f20c948e4529572
ThiagoSantos-devthb <thibbatista@gmail.com>
2021-12-25 15:08:54 -0300

    first commit

---
+++ src/test/java/com/thiagosantos/PersonapiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thiagosantos;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PersonapiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
