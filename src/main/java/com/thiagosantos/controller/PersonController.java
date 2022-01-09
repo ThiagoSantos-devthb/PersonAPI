ebba6e17d5b3df65b1f73601ae6aa7ca35704dba
ThiagoSantos-devthb <thibbatista@gmail.com>
2022-01-09 11:25:52 -0300

    develoeped list all method

--- src/main/java/com/thiagosantos/controller/PersonController.java
+++ src/main/java/com/thiagosantos/controller/PersonController.java
@@ -11,6 +11,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/people")
@@ -29,6 +30,12 @@ public class PersonController {
     @ResponseStatus(HttpStatus.CREATED)
     public MessageResponseDTO createPerson(@RequestBody @Valid PersonDTO personDTO){
       return personService.createPerson(personDTO);
+
+    }
+
+    @GetMapping
+    public List<PersonDTO> listAll(){
+       return personService.listAll();
 
     }
 }
